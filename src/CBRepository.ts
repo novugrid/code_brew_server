f1699554624d191620c69ce842b23886e42ca8ee
dammyololade <dammyololade2010@gmail.com>
2020-04-20 07:58:04 +0100

    fixed isues with pagination

--- src/CBRepository.ts
+++ src/CBRepository.ts
@@ -12,6 +12,15 @@ export class CBRepository<T extends Model<T>> {
     model!: NonAbstractTypeOfModel<T>
 
 
+    /**
+     * 
+     * @param req express request
+     * @param where : optionally include a where option for the query
+     * @param includeAble : model or association attached to the model (optional)
+     *                      Note when adding your includable model kindly set separate parameter of 
+     *                      each model to true so the count wont affect the total count of the result
+     * @param order : sorting order 
+     */
     public async paginate(req: express.Request, where: WhereOptions, 
         includeAble: Includeable[], order: []) {
         var res = new PaginationResponse()
@@ -30,12 +39,14 @@ export class CBRepository<T extends Model<T>> {
                 limit: pageSize,
                 order: order
             })
-            res.total_pages = result.count / pageSize
+            res.total = result.count
+            res.total_pages = Math.ceil(result.count / pageSize)
             res.current_page = page
-            res.data = result.rows
             res.limit = pageSize
-            res.previous_page = page == 1 ? 1 : (page - 1)
+            //res.previous_page = page == 1 ? 0 : (page - 1)
+            res.previous_page = (page - 1)
             res.next_page = page == res.total_pages ? res.total_pages : (page + 1)
+            res.data = result.rows
             return res;
         } catch(err) {
             console.log("error whule performing pagination", err)
