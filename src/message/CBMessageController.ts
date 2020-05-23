30226c8b689b7b0f3f0ec73269294bf45ff38eec
dammyololade <dammyololade2010@gmail.com>
2020-05-23 09:36:36 +0100

    fixed issues on message controller

--- src/message/CBMessageController.ts
+++ src/message/CBMessageController.ts
@@ -67,7 +67,7 @@ export class CBMessaageController{
         try {
             let messages = await CBMessage.findAll({
                 where: Sequelize.or(
-                    {userr_id: req.params.id},
+                    {user_id: req.params.id},
                     {message_type: MessageType.All}
                 ),
                 include: [imageQuery]
